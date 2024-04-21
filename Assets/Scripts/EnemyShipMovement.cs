5568e80498719bae0564f7e965db0280f09f67f5
Heemo2000 <hgupta01062000@gmail.com>
2024-04-21 16:15:27 +0530

    Initial implementation of enemy movement.

--- Assets/Scripts/EnemyShipMovement.cs
+++ Assets/Scripts/EnemyShipMovement.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 namespace Game
 {
-    public class EnemyShipMovement : MonoBehaviour
+    public class EnemyShipMovement : Movement
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
+        [Header("Enemy settings")]
+        [SerializeField]private Transform target;
+        [SerializeField]private float stoppingDistance;
 
         // Update is called once per frame
-        void Update()
+        protected override void Update()
         {
-        
+            float squareDistance = Vector2.SqrMagnitude(target.position - transform.position);
+            if(squareDistance > stoppingDistance * stoppingDistance)
+            {
+                Debug.Log("Following player");
+                Vector2 direction = (target.position - transform.position).normalized;
+                base.SailPos += direction.y * base.sailTurnSpeed;
+                RudderTurn += direction.x * rudderTurnSpeed;
+            }
+            base.Update();
         }
     }
 }
