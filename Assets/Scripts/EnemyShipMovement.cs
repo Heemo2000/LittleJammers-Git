3064de653952919e5b16fec45475c970e8aaffd2
Heemo2000 <hgupta01062000@gmail.com>
2024-05-01 18:52:30 +0530

    Made somewhat working enemy ship movement.

--- Assets/Scripts/EnemyShipMovement.cs
+++ Assets/Scripts/EnemyShipMovement.cs
@@ -9,19 +9,55 @@ namespace Game
         [Header("Enemy settings")]
         [SerializeField]private Transform target;
         [SerializeField]private float stoppingDistance;
+        [Min(0.0f)]
+        [SerializeField]private float maxRayCheckDistance = 100.0f;
+        [SerializeField]private LayerMask playerMask;
+        private bool _isTargetInFront;
+
+        private float _requiredRudderTurn = 0.0f;
 
         // Update is called once per frame
         protected override void Update()
         {
             float squareDistance = Vector2.SqrMagnitude(target.position - transform.position);
+            Debug.Log("Square Distance: " +squareDistance);
             if(squareDistance > stoppingDistance * stoppingDistance)
             {
-                Debug.Log("Following player");
+                base.isAnchored = false;
+                
                 Vector2 direction = (target.position - transform.position).normalized;
-                base.SailPos += direction.y * base.sailTurnSpeed;
-                RudderTurn += direction.x * rudderTurnSpeed;
+                
+                _requiredRudderTurn = direction.x == 0.0f ? 0.0f : Mathf.Sign(direction.x);
+                if(_isTargetInFront)
+                {
+                    Debug.DrawLine(transform.position, target.position, Color.green);
+                    _requiredRudderTurn = 0.0f;
+                }
+                else
+                {
+                    base.SailPos += base.sailTurnSpeed;    
+                }
+                if(_requiredRudderTurn == 0.0f)
+                {
+                    RudderTurn = 0.0f;
+                }
+                else
+                {
+                    RudderTurn += _requiredRudderTurn * base.rudderTurnSpeed;
+                }
+                
+            }
+            else
+            {
+                base.isAnchored = true;
             }
             base.Update();
+        }
+
+        private void FixedUpdate() 
+        {
+            RaycastHit2D hit = Physics2D.Linecast(transform.position, transform.position + transform.up * maxRayCheckDistance, playerMask.value);
+            _isTargetInFront = hit.collider != null;
         }
     }
 }
